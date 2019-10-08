b679e4dff9680b6299a19ab5060fd58b04b76c9c
suulcoder <ef.proyectomundial@gmail.com>
2019-10-07 20:58:00 -0600

    header

--- LostDataChaser.py
+++ LostDataChaser.py
@@ -1,1 +1,24 @@
 """
+-------------------------------------------------------------------------------
+	
+
+	Name: Chasers of the lost data
+	Format: .py
+	Autors: 
+		-Saúl Contreras (SuulCoder)
+		-Luis Ferando Quezada
+		-Marco Fuentes
+		-Justin Trigueros
+		-Jonathan Pu
+
+	Use: 
+
+		These program is useful to recover the data that has been lost by
+		some sensors of the Nasa. These is a projecto for the SpaceApp 
+		Challege 2019. This algorithm add that data with Machine Learning
+		process, libraries from Python and statistics regressions. 
+
+
+-------------------------------------------------------------------------------
+"""
+
