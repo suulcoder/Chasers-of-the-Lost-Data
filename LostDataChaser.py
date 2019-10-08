911449d899738ced2922cd80512cda60494370e8
suulcoder <ef.proyectomundial@gmail.com>
2019-10-08 14:00:19 -0600

    firsttest to sklearn ML library

--- LostDataChaser.py
+++ LostDataChaser.py
@@ -21,8 +21,15 @@
 
 -------------------------------------------------------------------------------
 """
-
+#Import all libraries
 import csv
+import numpy as np
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+#------------------------------------------------------------------------------
+
+
+#FUNCTIONS:
 
 # given a csv file, returns an array with each event/object in a dictionary with all it's elements named
 def dataParser(fileName):
@@ -48,12 +55,28 @@ def printData(data):
 			print(f'{title}: {info}')
 		print('\n')
 
+#------------------------------------------------------------------------------
+imp = IterativeImputer(max_iter=10, random_state=0)
+imp.fit([[1, 2], [3, 6], [4, 8], [np.nan, 3], [7, np.nan]])  
+IterativeImputer(add_indicator=False, estimator=None,
+                 imputation_order='ascending', initial_strategy='mean',
+                 max_iter=10, max_value=None, min_value=None,
+                 missing_values=nan, n_nearest_features=None,
+                 random_state=0, sample_posterior=False, tol=0.001,
+                 verbose=0)
+X_test = [[np.nan, 2], [6, np.nan], [np.nan, 6]]
+ # the model learns that the second feature is double the first
+ print(np.round(imp.transform(X_test)))
+
+
 # Parsed info of the csv's
+
+fireballAndBolideReports = dataParser('Fireball_And_Bolide_Reports.csv')
+printData(fireballAndBolideReports)
+
+"""
 meteorLandings = dataParser('Meteorite_Landings.csv')
 printData(meteorLandings)
-
-fireballAndBolideReports = dataParser('FireBall_And_Bolide_Reports.csv')
-printData(fireballAndBolideReports)
 
 GLC03122015 = dataParser('GLC03122015.csv')
 printData(GLC03122015)
@@ -61,3 +84,4 @@ printData(GLC03122015)
 NearEarthCometsOrbitalElements = dataParser('Near-Earth_Comets_-_Orbital_Elements.csv')
 printData(NearEarthCometsOrbitalElements)
 
+"""
