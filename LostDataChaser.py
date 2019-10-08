65629fa26a8dbf462077cf1f347d5a2039449f4e
SuulCoder <suulcoder@gmail.com>
2019-10-07 23:48:26 -0600

    Update LostDataChaser.py

--- LostDataChaser.py
+++ LostDataChaser.py
@@ -13,7 +13,7 @@
 
 	Use: 
 
-		These program is useful to recover the data that has been lost by
+		This program is useful to recover the data that has been lost by
 		some sensors of the Nasa. These is a projecto for the SpaceApp 
 		Challege 2019. This algorithm add that data with Machine Learning
 		process, libraries from Python and statistics regressions. 
